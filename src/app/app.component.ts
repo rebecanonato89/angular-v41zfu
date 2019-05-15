bb9244f647ee99a912a2677d343abaf950e28b15
REBECANONATO <rebecanonato89@gmail.com>
2019-05-15 16:48:10 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,12 @@ export class AppComponent  {
   name = 'Angular';
   formConcluidoErro: boolean = false;
   formConcluido: boolean = false;
+  resp = "Está com algum erro!!!";
   
   inserir(){
     this.formConcluido = true;
+  }
+  inserir2(){
+    this.formConcluidoErro = true;
   }
 }
